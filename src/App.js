82d9856bcd7f246c1f714c8ff22168fcfd5c1a02
Christina Joshi <christinajoshi1@gmail.com>
2022-04-28 15:30:08 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import Counter from "./classComponentCounter";
 import CounterHooks from "./functionComponentCounter";
 
 /* Context is primarily used when some data needs to be accessible by *many* components at different nesting levels without having to pass props
-    down manually. Use sparingly because it makes component reuse more difficult
+    down manually. Use sparingly because it makes component reuse more difficult.
     Contexts have a provider and a consumer. Provider is allowing us to pass out a value and the consumer is just consuming that value.
     To use wrap all code want access to into the context.
  */   
 export const ThemeContext = React.createContext()
 
-// no longer need import statement? import React from 'react';
+// no longer need import statement? import React from 'react'
 // rendering all the application code inside of root
 // function can only return one thing. if want to return more, use JSX fragments <> ... </>
 function App() {
